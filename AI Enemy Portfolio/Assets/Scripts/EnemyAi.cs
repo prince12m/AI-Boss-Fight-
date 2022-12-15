5240b2fb0f48fcedfd2e3730a73be7d71717891b
prince12m <pmalonga2021@elam.co.uk>
2022-12-15 10:52:57 +0000

    Yh

--- AI Enemy Portfolio/Assets/Scripts/EnemyAi.cs
+++ AI Enemy Portfolio/Assets/Scripts/EnemyAi.cs
@@ -11,6 +11,10 @@ public class EnemyAi : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
+    public GameObject projectile;
+
+    public float health;
+
     //Patroliing
     public Vector3 walkPoint;
     bool walkPointSet;
@@ -77,6 +81,44 @@ public class EnemyAi : MonoBehaviour
         //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
-        
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code here
+
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 5f);
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
